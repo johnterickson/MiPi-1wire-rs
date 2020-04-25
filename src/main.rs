62d364f002680f6d5b47f0c5cfddb196c5e810df
John Erickson <john.t.erickson@gmail.com>
2020-04-25 11:47:16 -0700

    actually try to read

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use tokio::io::AsyncReadExt;
 use std::convert::Infallible;
-use std::{error::Error, net::SocketAddr};
+use std::{error::Error, net::SocketAddr, fs::File, io::{BufRead, BufReader}};
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Method, StatusCode};
-use tokio::fs::File;
 
 async fn get_temps() -> Result<String,Box<dyn Error>> {
-    let mut file = File::open("/sys/devices/w1_bus_master1/w1_master_slave_count").await?;
-    let mut contents = vec![];
-    file.read_to_end(&mut contents).await?;
-
     let mut body = format!("<a updated='{}'>\n", "2020-04-24 21-28");
 
-    body += "<owd>\n";
-    body += "<Name>DS18B20</Name>\n";
-    body += &format!("<ROMId>{}</ROMId>\n","28-03119779ae01");
-    body += &format!("<Temperature>{}</Temperature>\n","24.0");
-    body += "</owd>\n";
+    let mut file = File::open("/sys/devices/w1_bus_master1/w1_master_slaves")?;
+    for id in BufReader::new(file).lines() {
+        let id = id?;
+
+        let path = format!("/sys/bus/w1/devices/{}/w1_slave", id);
+        let mut lines = BufReader::new(File::open(path)?).lines();
+        lines.next().unwrap()?;
+        let data: &str = &lines.next().unwrap()?;
+        let mut tokens = data.split("=");
+        tokens.next().unwrap(); // ignore
+        let temp = i32::from_str_radix(tokens.next().unwrap(), 10)?;
+        let temp = temp as f32;
+        let temp = temp / 1000.0;
+        body += "<owd>\n";
+        body += "<Name>DS18B20</Name>\n";
+        body += &format!("<ROMId>{}</ROMId>\n",id);
+        body += &format!("<Temperature>{}</Temperature>\n",temp);
+        body += "</owd>\n";
+    }
 
     body += "</a>\n";
+
+    println!("{}", &body);
 
     Ok(body)
 }
